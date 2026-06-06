07f6aba6aadf57851ec21d3fae7dc0a1799716e2
Paulina Vvedenskaya <pv82@duke.edu>
2026-06-05 22:47:00 -0400

    NeetCode: sync 2 submissions

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ret;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> currCombo;
+        sort(nums.begin(), nums.end());
+        dfs(nums, target, currCombo, 0);
+        return ret;
+    }
+
+    void dfs(vector<int>& nums, int target, vector<int>& currCombo, int currMax) {
+        if (target == 0) {
+            ret.push_back(currCombo); // good combo
+            return;
+        }
+        for (int n : nums) {
+            if(target - n < 0){
+                return;
+            }
+            if (n >= currMax) {
+                currCombo.push_back(n);
+                dfs(nums, target - n, currCombo, n);
+                currCombo.pop_back();
+            }
+        }
+    }
+};
