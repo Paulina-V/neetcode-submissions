8ffd28afe024574035453d7b5de2b95cab0dd619
Paulina Vvedenskaya <pv82@duke.edu>
2026-07-10 17:22:00 -0400

    NeetCode: sync 2 submissions

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int res = nums[0];
+
+        int productL = 1;
+        int productR = 1;
+
+        for (int i = 0; i < n; i++) {
+            productL *= nums[i];
+
+            if (productL > res) {
+                res = productL;
+            }
+
+            if (nums[i] == 0) {
+                productL = 1;
+                if (res <= 0) {
+                    res = 0;
+                }
+            }
+
+            int j = n - 1 - i;
+            productR *= nums[j];
+            if (productR > res) {
+                res = productR;
+            }
+
+            if (nums[j] == 0) {
+                productR = 1;
+                if (res < 0) {
+                    res = 0;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
